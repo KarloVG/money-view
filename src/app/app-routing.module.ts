776cd3069394119beb87ba3bd7b4c8a88f39211d
Ivan Benko <ivan.benko@fleksbit.hr>
2020-10-14 14:52:32 +0200

    chore(routing): Add AccountStatementSummaryList module to app.routing for lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    // tslint:disable-next-line:max-line-length
+    loadChildren: () => import('./features/account-statement-summary-list/account-statement-summary-list.module').then(m => m.AccountStatementSummaryListModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
