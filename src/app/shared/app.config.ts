fcd232fb74754a305f8526e9f2c8555e16eb1de7
Ivan Benko <ivan.benko@fleksbit.hr>
2020-10-30 17:13:16 +0100

    chore: config host route

--- src/app/shared/app.config.ts
+++ src/app/shared/app.config.ts
@@ -11,7 +11,7 @@ interface Api {
 
 const app: App = {
   Api: {
-    rootUrl: new URL(environment.production ? 'https://mv-dev.fleksbit.org/app/api' : 'https://localhost:5021/app/api')
+    rootUrl: new URL(environment.production ? 'https://mv-dev.fleksbit.org/host' : 'https://localhost:5021/host')
   },
   DefaultDebounce_ms: 300
 };
