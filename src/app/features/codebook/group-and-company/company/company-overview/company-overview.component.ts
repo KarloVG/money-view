298fe4b5f2ea8725e6bedb7714eb6270cacfca87
David Ceranic <david.ceranic@fleksbit.hr>
2020-12-16 13:57:24 +0100

    Center all modals

--- src/app/features/codebook/group-and-company/company/company-overview/company-overview.component.ts
+++ src/app/features/codebook/group-and-company/company/company-overview/company-overview.component.ts
@@ -10,16 +10,18 @@ import { IFleksbitResponse } from 'src/app/shared/models/fleksbit-response';
 import { PageInfo } from 'src/app/shared/page-info';
 import { NotificationService } from 'src/app/shared/services/swal-notification/notification.service';
 import { ModalAoeCompanyComponent } from '../modal-aoe-company/modal-aoe-company.component';
-import { IPaginatedResponseCompany, IResponseCompany } from '../models/response/response-company';
+import {
+  IPaginatedResponseCompany,
+  IResponseCompany,
+} from '../models/response/response-company';
 import { CompanyService } from '../services/company.service';
 
 @Component({
   selector: 'mv-company-overview',
   templateUrl: './company-overview.component.html',
-  styleUrls: ['./company-overview.component.scss']
+  styleUrls: ['./company-overview.component.scss'],
 })
 export class CompanyOverviewComponent implements OnInit {
-
   isSubmitLoaderActive: boolean = false;
   rows: IResponseCompany[] = [];
   loadingIndicator: boolean = true;
@@ -33,7 +35,7 @@ export class CompanyOverviewComponent implements OnInit {
     private _companyService: CompanyService,
     private _notificationService: NotificationService,
     private _spinner: NgxSpinnerService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getCompanies();
@@ -47,11 +49,17 @@ export class CompanyOverviewComponent implements OnInit {
 
   // Get all
   getCompanies(): void {
-    this._companyService.get(this.desiredPageOffset, this.desiredPageSize).pipe(
+    this._companyService
+      .get(this.desiredPageOffset, this.desiredPageSize)
+      .pipe(
         take(1),
-        catchError(err => this.catchAndReplaceError(err)),
-        map((response: IFleksbitResponse<IPaginatedResponseCompany>) => response.response),
-      ).subscribe((res: IPaginatedResponseCompany) => {
+        catchError((err) => this.catchAndReplaceError(err)),
+        map(
+          (response: IFleksbitResponse<IPaginatedResponseCompany>) =>
+            response.response
+        )
+      )
+      .subscribe((res: IPaginatedResponseCompany) => {
         this.rows = res.data;
         this.loadingIndicator = false;
         this.currentEntryCount = res.pagination.count;
@@ -60,59 +68,75 @@ export class CompanyOverviewComponent implements OnInit {
 
   // Add or Edit
   addOrEditCompany(editCompany?: IResponseCompany): void {
-    const modal = this._modal.open(ModalAoeCompanyComponent, { size: 'l', backdrop: 'static' });
+    const modal = this._modal.open(ModalAoeCompanyComponent, {
+      size: 'l',
+      backdrop: 'static',
+      centered: true,
+    });
     if (editCompany) {
-      modal.componentInstance.companyEdit = editCompany
+      modal.componentInstance.companyEdit = editCompany;
     }
-    modal.result.then(result => {
-      if (result && result.id) {
-        this._companyService.put(result).pipe(
-          take(1),
-           catchError(err => this.catchAndReplaceError(err))
-          ).subscribe(
-          data => {
-            this.handleSuccesResponse("Firma je uređena");
-          }
-        )
-      } else {
-        this._companyService.add(result).pipe(
-          take(1),
-           catchError(err => this.catchAndReplaceError(err))
-          ).subscribe(
-          data => {
-            this.handleSuccesResponse("Firma je dodana");
-          }
-        )
-      }
-    }).catch(reason => {
-      if (editCompany) {
-        this.handleModalDismiss("Firma nije uređena");
-      } else {
-        this.handleModalDismiss("Firma nije dodana");
-      }
-      // todo swift alert warning
-    })
+    modal.result
+      .then((result) => {
+        if (result && result.id) {
+          this._companyService
+            .put(result)
+            .pipe(
+              take(1),
+              catchError((err) => this.catchAndReplaceError(err))
+            )
+            .subscribe((data) => {
+              this.handleSuccesResponse('Firma je uređena');
+            });
+        } else {
+          this._companyService
+            .add(result)
+            .pipe(
+              take(1),
+              catchError((err) => this.catchAndReplaceError(err))
+            )
+            .subscribe((data) => {
+              this.handleSuccesResponse('Firma je dodana');
+            });
+        }
+      })
+      .catch((reason) => {
+        if (editCompany) {
+          this.handleModalDismiss('Firma nije uređena');
+        } else {
+          this.handleModalDismiss('Firma nije dodana');
+        }
+        // todo swift alert warning
+      });
   }
 
   // Delete
   deleteCompany(company: IResponseCompany): void {
-    const modalRef = this._modal.open(ConfirmationModalComponent, { backdrop: 'static', keyboard: false });
+    const modalRef = this._modal.open(ConfirmationModalComponent, {
+      backdrop: 'static',
+      keyboard: false,
+      centered: true,
+    });
     modalRef.componentInstance.title = 'Brisanje firme';
     modalRef.componentInstance.description = `Želite li obrisati firmu pod nazivom ${company.name}?`;
     modalRef.componentInstance.isDelete = true; // text danger
-    modalRef.result.then( result => {
-      if(result == true) {
-        this._companyService.delete(company.id).pipe(
-          take(1),
-           catchError(err => this.catchAndReplaceError(err))
-        ).subscribe(
-          data => {
-            this.handleSuccesResponse("Firma je obrisana");
-        });
-      }
-    }).catch(reason => {
-      this.handleModalDismiss("Firma nije obrisana");
-    })
+    modalRef.result
+      .then((result) => {
+        if (result == true) {
+          this._companyService
+            .delete(company.id)
+            .pipe(
+              take(1),
+              catchError((err) => this.catchAndReplaceError(err))
+            )
+            .subscribe((data) => {
+              this.handleSuccesResponse('Firma je obrisana');
+            });
+        }
+      })
+      .catch((reason) => {
+        this.handleModalDismiss('Firma nije obrisana');
+      });
   }
 
   // 201 - Success
@@ -123,7 +147,7 @@ export class CompanyOverviewComponent implements OnInit {
       this._spinner.hide();
       this._notificationService.fireSuccessMessage(successMessage);
       this.getCompanies();
-    }, 500)
+    }, 500);
   }
 
   // Error handling
