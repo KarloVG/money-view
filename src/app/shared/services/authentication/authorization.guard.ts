bedb75059192f9424acfff49f1abd9be39ebc18e
David Ceranic <david.ceranic@fleksbit.hr>
2021-01-08 16:22:05 +0100

    Quick fix

--- src/app/shared/services/authentication/authorization.guard.ts
+++ src/app/shared/services/authentication/authorization.guard.ts
@@ -1,38 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Router, CanLoad, Route } from '@angular/router';
-import { first } from 'rxjs/operators';
-import { ApiClientService, UserInfoResponse } from '../api-client/api-client.service';
+import { Observable } from 'rxjs';
+import { first, map } from 'rxjs/operators';
+import { ApiClientService } from '../api-client/api-client.service';
 import { NotificationService } from '../swal-notification/notification.service';
 
 @Injectable()
 export class AuthorizationGuardService implements CanLoad {
-    userInformations!: UserInfoResponse | null;
+
     constructor(
       private _router: Router,
       private _apiClient: ApiClientService,
       private _notificationService: NotificationService
-    ) {
-      this._apiClient.userInfo().pipe(first()).subscribe(response => this.userInformations = response);
-    }
+    ) {  }
 
-    canLoad(route: Route): boolean {
-      if (this.userInformations?.role == 'admin')  {
-        // admin
-        if(route.path === '') {
-          this._router.navigate(['codeboook/user-panel']);
-          return false;
+    canLoad(route: Route):Observable<boolean>|boolean {
+      return this._apiClient.userInfo().pipe(map(
+        response => {
+          if (response?.role == 'admin')  {
+            // admin
+            if(route.path === '') {
+              this._router.navigate(['codeboook/user-panel']);
+              return false;
+            }
+            return true;
+          } else if(response?.role == 'group-manager' || response?.role == 'firm-manager') {
+            //manager groupe ili firme
+            if(route.path !== '') {
+              this._router.navigate(['']);
+              return false;
+            }
+            return true;
+          } else {
+              this._notificationService.fireErrorNotification("Ne možete pristupiti navedenoj stranici");
+              return false;
+          }
         }
-        return true;
-      } else if(this.userInformations?.role == 'group-manager' || this.userInformations?.role == 'firm-manager') {
-        //manager groupe ili firme
-        if(route.path !== '') {
-          this._router.navigate(['']);
-          return false;
-        }
-        return true;
-      } else {
-           this._notificationService.fireErrorNotification("Ne možete pristupiti navedenoj stranici");
-          return false;
-      }
+      ),
+      first())
     }
 }
