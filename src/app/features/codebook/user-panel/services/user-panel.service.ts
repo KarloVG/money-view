fcecdc4a0897efbf7c12f931d3148c1b39afcdae
Ivan Benko <ivan.benko@fleksbit.hr>
2021-01-12 09:26:39 +0100

    fix: ng build complained so we'll check for invalid user IDs before creating and sending a PUT request to update the user

--- src/app/features/codebook/user-panel/services/user-panel.service.ts
+++ src/app/features/codebook/user-panel/services/user-panel.service.ts
@@ -1,13 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { IFleksbitResponse } from 'src/app/shared/models/fleksbit-response';
-import { AppRouteService } from 'src/app/shared/services/route/app-route.service';
-import { environment } from '../../../../../environments/environment';
-import { IRequestUserPanel } from '../models/request/request-user-panel';
-import { IRequestRole } from '../models/request/role-request';
-import { ROLES } from '../models/request/roles';
-import { IResponseUserPanel } from '../models/response/response-user-panel';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {IFleksbitResponse} from 'src/app/shared/models/fleksbit-response';
+import {AppRouteService} from 'src/app/shared/services/route/app-route.service';
+import {environment} from '../../../../../environments/environment';
+import {IRequestUserPanel} from '../models/request/request-user-panel';
+import {IRequestRole} from '../models/request/role-request';
+import {ROLES} from '../models/request/roles';
+import {IResponseUserPanel} from '../models/response/response-user-panel';
 
 @Injectable({
   providedIn: 'root'
@@ -19,8 +19,9 @@ export class UserPanelService {
 
   constructor(
     private readonly _http: HttpClient,
-    private readonly _appRoute: AppRouteService,
-  ) { }
+    private readonly _appRoute: AppRouteService
+  ) {
+  }
 
   add(formGroup: IRequestUserPanel): Observable<IFleksbitResponse<IResponseUserPanel>> {
     const request = {
@@ -35,12 +36,17 @@ export class UserPanelService {
 
   //Edit existing
   put(formGroup: IRequestUserPanel): Observable<IFleksbitResponse<IResponseUserPanel>> {
+    if (!formGroup.id) {
+      throw new Error('Invalid User ID');
+    }
+
     const request = {
       email: formGroup.email,
       username: formGroup.username,
       firmId: formGroup.company,
-      roleNames: [this.roles.find(x => x.id == formGroup.role)?.name]
+      roleNames: [this.roles.find(x => x.id === formGroup.role)?.name]
     };
+
     const url = this._appRoute.createAppRouteURL([this.CONTROLLER_NAME, formGroup.id.toString()]);
     return this._http.put<IFleksbitResponse<IResponseUserPanel>>(url.toString(), request);
   }
