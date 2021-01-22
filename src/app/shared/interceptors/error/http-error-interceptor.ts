f148561a7b81b93af3544f56f083874f72092853
Ivan Benko <ivan.benko@fleksbit.hr>
2021-01-22 15:34:56 +0100

    fix: invalid type inference

--- src/app/shared/interceptors/error/http-error-interceptor.ts
+++ src/app/shared/interceptors/error/http-error-interceptor.ts
@@ -1,17 +1,17 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
-import { AuthenticationService } from '../../services/authentication/authentication.service';
-import { NotificationService } from '../../services/swal-notification/notification.service';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {environment} from 'src/environments/environment';
+import {AuthenticationService} from '../../services/authentication/authentication.service';
+import {NotificationService} from '../../services/swal-notification/notification.service';
 
 interface FleksbitError {
   code: number;
-  response: any,
+  response: object;
   error: {
     message: string
-  }
+  };
 }
 
 @Injectable()
@@ -36,15 +36,16 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
       .pipe(
-        catchError((error: FleksbitError) => {
-          if (error.code === 401) {
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) {
             this._authenticationService.login();
           }
           // Don't fire notification service
           if ((request.method === 'POST' || request.method === 'PUT') && this.isValidRequestForInterceptor(request.url)) {
             this._notificationService.fireErrorNotification(error.error.message);
           }
-          return throwError(error.error.message);
+          const fleksbitError = error.error as FleksbitError;
+          return throwError(fleksbitError?.error?.message ?? error.message);
         })
       );
   }
